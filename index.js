6ac521d1c5b8664dac3d756f31bc2ad1eaf6f396
Pratuish <pratuisharyajha123@gmail.com>
2026-01-01 14:11:25 +0530

    Fix CORS and API endpoints for deployment

--- index.js
+++ index.js
@@ -34,10 +34,11 @@ app.use(express.json());
 
 // Allowed frontend origins
   const allowedOrigins = [
-  "https://frontend1-maqwtl9iq-pjha9256s-projects.vercel.app",
+  "https://frontend1-k4r9tdd6a-pjha9256s-projects.vercel.app",
+,
   "http://localhost:3000"
 ];
- 
+
 
 // CORS Configuration
 app.use(
